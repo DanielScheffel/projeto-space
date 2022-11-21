474abe063ce3b155aa704daa2a5aa30fcf815ef2
DanielScheffel <109425652+DanielScheffel@users.noreply.github.com>
2022-11-21 14:58:31 -0300

    Banco de Dados

---
+++ banco.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA IF NOT EXISTS `dbprojetofinal` DEFAULT CHARACTER SET utf8 ;
+USE `dbprojetofinal` ;
+
+CREATE TABLE IF NOT EXISTS `dbprojetofinal`.`Usuario` (
+  `idUsuario` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`idUsuario`))
+ENGINE = InnoDB;
+
+insert into adminstrador(idUsuario, nome, email, senha) values (
+	1,
+    'Daniel',
+    'daniel-scheffel@gmail.com',
+    '9a909518ca4ab1d7ef37cdff516e680549143efb'
+);
+
+
+CREATE TABLE IF NOT EXISTS `dbprojetofinal`.`adminstrador` (
+  `idadminstrador` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`idadminstrador`))
+ENGINE = InnoDB;
+
+insert into adminstrador(idAdminstrador, nome, email, senha) values (
+	1,
+    'Luciano',
+    'luciano-neto@gmail.com',
+    '7fc84ca82177a8b84c905ca136490f1a9461b1b6'
+);
